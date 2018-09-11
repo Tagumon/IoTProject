4b61cf74f878aaf6c55f3838153494d317f4db69
K_TAGUMA_PC\Kouta <koh.tagu@outlook.jp>
2018-09-11 12:10:41 +0900

    研究室回転 リセット仮実装

---
+++ IoTProject/Assets/Scripts/ResetButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject LabObject;
+	
+	public void Reset(){
+		LabObject.transform.Rotate(0, 0, 0);
+	}
+}
